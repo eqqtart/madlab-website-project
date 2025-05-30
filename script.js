6672e1b0c93f6d034d00e58a78b6b63e11903a7f
eqqtart <jasmine.a.ly135@hotmail.com>
2025-05-30 22:15:47 +0100

    add html, css and js code for cards

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-function toggleCard(card) {
-  card.classList.toggle('open');
+  function openCard(clickedCard) {
+  // locate and select all cards
+  const allCards = document.querySelectorAll('.profileCard');
+
+  // loop through each card
+  for (let card of allCards) {
+    // if the card is not the clicked one, collapse it
+    if (card !== clickedCard) {
+      card.classList.remove('expanded');
+    }
+  }
+
+  // clicked card toggle (expand if collapsed, collapse if expanded)
+  clickedCard.classList.toggle('expanded');
 }
