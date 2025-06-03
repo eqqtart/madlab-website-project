9e66ac9bab29d671ee65cc380f3ec73fa7ef1b7d
eqqtart <jasmine.a.ly135@hotmail.com>
2025-06-03 01:55:34 +0100

    change logo and card images, add information in profile cards, improve form functions in script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// CARD FUNCTIONS
   function openCard(clickedCard) {
   // locate and select all cards
   const allCards = document.querySelectorAll('.profileCard');
@@ -13,3 +14,41 @@
   // clicked card toggle (expand if collapsed, collapse if expanded)
   clickedCard.classList.toggle('expanded');
 }
+
+// JOIN US FUNCTIONS
+const form = document.forms.signupForm;
+const elements = form.elements;
+
+function getValueByName(elementName) {
+  return elements[elementName].value;
+}
+
+  // waht to do
+  function handleSubmit(event) {
+    console.log(event);
+    event.preventDefault();
+
+  // get application details
+  const email = getValueByName("email");
+  const name = getValueByName("name");
+  const islandName = getValueByName("islandName");
+  const dreamAddy = getValueByName("dreamAddy");
+  const fruit = getValueByName("fruit");
+  const villagers = getValueByName("villager");
+
+  console.log(email+name+islandName+dreamAddy+fruit+villagers);
+
+
+  // show application form
+  alert(`
+Application Sent!
+
+Thank you ${name} for your application! We shall send an email to ${email} once we've reviewed your form and created your character card!
+    `);
+
+    // reset form after submit
+    form.reset();
+}
+
+// listen for user
+form.addEventListener("submit", handleSubmit);
